9bfc9854409c4d61f863a90906bb776213b7232e
Do Hyun Myung <dohymywork@gmail.com>
2023-04-05 16:32:43 +0900

    1

---
+++ 05.함수와파일/wordcount.py
@@ -0,0 +1,25 @@
+# 파일의 단어 개수 세기
+import re, string, sys
+
+# sys.argv[0] - WordCount, sys.argv[1] - filename
+if len(sys.argv) != 2:
+    print('사용법 : WordCount filename')
+    sys.exit(-1)
+
+with open(sys.argv[1]) as file:
+    contents = file.read()
+clean_contents = re.sub('['+string.punctuation+']', ' ', contents)
+clean_contents = clean_contents.replace('\n', ' ').lower()
+words = clean_contents.split()
+print(f'총단어는 {len(words):,d}개 이고, 고유단어는 {len(set(words)):,d}개입니다.')
+
+dic = {}
+for word in words:
+    if word in dic.keys():
+        dic[word] +=1
+    else:
+        dic[word] = 1
+result= sorted(dic.items(), key=lambda x: x[1], reverse=True)
+print('사용빈도가 높은 단어 top10')
+for word, count in result[:10]:
+    print(f'{word}\t{count:,d}')
